9393d657c6074852059a1bc9a32b561b8510a854
Bjarnebrur <cadok9842@usn.no>
2025-09-15 21:37:13 +0200

    test 2

---
+++ quiz/02.php
@@ -0,0 +1,16 @@
+$tall1=4;
+$tall2=5;
+
+if($tall1==$tall2)
+{
+    print("Tallene er like");
+}
+    else if ($tall1!=$tall2)
+{
+    print("tallene er ikke like");
+}
+else
+{  
+    print("tallene er ikke mulig å avgjøre om er like")
+
+}
